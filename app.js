9b358d5b758e58c21bbd15fdcd893bf9787278cf
parvejme24 <mdparvejme24@gmail.com>
2024-03-16 22:01:22 +0600

    modify app.js file

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const express = require("express");
 const app = express();
 const router = require("./src/api");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // security middleware import
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const limier = rateLimit({ windowMs: 60 * 60 * 1000, limit: 1000 });
+const cors = require("cors");
+const corsOptions = {
+  origin: ["*"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 // security middlware use
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(helmet());
 app.use(mongoSanitize());
@@ -22,9 +28,19 @@ app.use(hpp());
 app.use(xss());
 app.use(limier);
 
+// non security middleware use
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
 
-// api endpoints
+// home route
+app.use("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Munshi-Wholesale Server" });
+});
+
+// all api endpoints
 app.use("/api/v1", router);
 
 app.get("*", (req, res) => {
