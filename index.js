4cbe9935c5b6b95b42f8365c1613d822938b8004
parvejme24 <mdparvejme24@gmail.com>
2024-03-23 03:09:31 +0600

    use server port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const app = require("./app");
 // const connectDB = require("./src/config/db");
-// const { serverPort } = require("./src/secret");
+const { serverPort } = require("./src/secret");
 
 // // Start the server
 // app.listen(serverPort, async () => {
@@ -8,18 +8,19 @@
 //   console.log(`Munshi Wholesale is running at http://localhost:${serverPort}`);
 // });
 
-
 require("dotenv").config;
 const http = require("http");
 const app = require("./app");
 const connectToDB = require("./src/config/db");
 const server = http.createServer(app);
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 const main = async () => {
   await connectToDB();
-  server.listen(port, () => {
-    console.log(`Munshi Wholesale server is running on: http://localhost:${port}`);
+  server.listen(serverPort, () => {
+    console.log(
+      `Munshi Wholesale server is running on: http://localhost:${serverPort}`
+    );
   });
 };
 
