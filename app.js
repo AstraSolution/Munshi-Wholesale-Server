469906e53023587745dcfb62fde228dc1a7b9c28
parvejme24 <mdparvejme24@gmail.com>
2024-03-22 14:42:55 +0600

    organize app.js file

--- app.js
+++ app.js
@@ -6,54 +6,57 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const createError = require("http-errors");
 
-// security middleware import
+// Security middleware imports
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const hpp = require("hpp");
 const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
-const limier = rateLimit({ windowMs: 60 * 60 * 1000, limit: 1000 });
 const cors = require("cors");
 
+// Configure rate limiting
+const limiter = rateLimit({ windowMs: 60 * 60 * 1000, limit: 1000 });
+
+// CORS options
 const corsOptions = {
   origin: ["*", "http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
 };
 
-// security middlware use
+// Use security middleware
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(hpp());
 app.use(xss());
-app.use(limier);
+app.use(limiter);
 
-// non security middleware use
+// Use non-security middleware
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cookieParser());
 
-// all api endpoints
+// API routes
 app.use("/api/v1", router);
 
-// home route
-app.use("/", (req, res) => {
+// Home route
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Munshi-Wholesale Server" });
 });
 
+// Invalid URL handler
 app.get("*", (req, res) => {
-  res.status(401).json({ message: "Invalid URL" });
+  res.status(404).json({ message: "Invalid URL" });
 });
 
-// client site error
+// Client-side error handler
 app.use((req, res, next) => {
-  next(createError(404, { message: "route not found" }));
+  next(createError(404, { message: "Route not found" }));
 });
 
-// server error
+// Server error handler
 app.use((err, req, res, next) => {
   return res.status(err.status || 500).json({
     success: false,
