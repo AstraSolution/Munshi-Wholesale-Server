fe5079d602792b5e2922c87fcf6a7093f73e8647
parvejme24 <mdparvejme24@gmail.com>
2024-03-23 03:12:36 +0600

    modify api.js fiel

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// const app = require("./app");
-// const connectDB = require("./src/config/db");
 const { serverPort } = require("./src/secret");
-
-// // Start the server
-// app.listen(serverPort, async () => {
-//   await connectDB();
-//   console.log(`Munshi Wholesale is running at http://localhost:${serverPort}`);
-// });
-
 require("dotenv").config;
 const http = require("http");
 const app = require("./app");
 const connectToDB = require("./src/config/db");
 const server = http.createServer(app);
-// const port = process.env.PORT || 5000;
 
 const main = async () => {
   await connectToDB();
