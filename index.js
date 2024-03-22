fe3f97476cde5781d2d8977985b50400983b5660
parvejme24 <mdparvejme24@gmail.com>
2024-03-22 14:45:37 +0600

    organize index.js file

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const app = require("./app");
 const connectDB = require("./src/config/db");
 const { serverPort } = require("./src/secret");
 
-app.listen(serverPort, async() => {
-  console.log(`Munshi Wholesale is running at http://localhost:${serverPort} `);
-  await connectDB()
+// Start the server
+app.listen(serverPort, async () => {
+  await connectDB();
+  console.log(`Munshi Wholesale is running at http://localhost:${serverPort}`);
 });
